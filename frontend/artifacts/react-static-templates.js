825047fc094acf87d427d06986e3aa122ce48344
Artem Kamyshansky <friendka@gmail.com>
2020-01-06 20:52:44 +0300

    Initial frontend app (react-static init)

---
+++ frontend/artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/opt/app/src/pages/404.js': require('/opt/app/src/pages/404.js').default,
+'/opt/app/src/pages/about.js': require('/opt/app/src/pages/about.js').default,
+'/opt/app/src/pages/blog.js': require('/opt/app/src/pages/blog.js').default,
+'/opt/app/src/pages/index.js': require('/opt/app/src/pages/index.js').default,
+'/opt/app/src/containers/Post': require('/opt/app/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/opt/app/src/pages/404.js'
+
